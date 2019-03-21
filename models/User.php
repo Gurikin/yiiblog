6bee1c1140828b5474c3d4a3cb64e7b32e2cc4c8
Gurikin <igor.banchikov@gmail.com>
2019-03-21 07:28:30 +0300

    Authenticate complete repaire gitignore

---
+++ models/User.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\BaseObject;
+use yii\base\Exception;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\web\IdentityInterface;
+
+
+/**
+ * This is the model class for table "{{%user}}".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string $email
+ * @property string $profile
+ *
+ * @property Post[] $posts
+ */
+class User extends ActiveRecord implements IdentityInterface
+{
+    private $authKey;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%user}}';
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'email'], 'required'],
+            [['profile'], 'string'],
+            [['username', 'password', 'email'], 'string', 'max' => 128],
+        ];
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'email' => 'Email',
+            'profile' => 'Profile',
+        ];
+    }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPosts()
+    {
+        return $this->hasMany(Post::className(), ['author_id' => 'id']);
+    }
+
+    /**
+     * @param int|string $id
+     * @return User|null|IdentityInterface
+     */
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id]);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    public static function findByUsername($username)
+    {
+        return static::findOne(['username' => $username]);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+    /**
+     * @param string $authKey
+     * @return bool
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        if (Yii::$app->getSecurity()->validatePassword($password, $this->password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
